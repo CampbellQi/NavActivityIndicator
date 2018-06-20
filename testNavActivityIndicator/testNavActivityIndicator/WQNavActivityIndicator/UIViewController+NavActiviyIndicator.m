c41ab4e61c733a5cdcfb515155c1c83f23aa45b8
fengwanqi <545425758@qq.com>
2018-06-20 14:16:52 +0800

    重构

--- testNavActivityIndicator/testNavActivityIndicator/WQNavActivityIndicator/UIViewController+NavActiviyIndicator.m
+++ testNavActivityIndicator/testNavActivityIndicator/WQNavActivityIndicator/UIViewController+NavActiviyIndicator.m
@@ -7,24 +7,17 @@
 //
 
 #import "UIViewController+NavActiviyIndicator.h"
-#import "testNavActivityIndicator-Swift.h"
+#import "Housekeeping-Swift.h"
 
 @implementation UIViewController (NavActiviyIndicator)
 - (void)showNavActivityIndicator {
-    if ([self.navigationItem.titleView isKindOfClass:WQNavActivityIndicatorView.class] == false) {
-        WQNavActivityIndicatorView *nai = [[WQNavActivityIndicatorView alloc] initWithFrame:CGRectZero title:self.title];
-        self.navigationItem.titleView = nai;
-        
-        NSDictionary *titleTextAttributes = self.navigationController.navigationBar.titleTextAttributes;
-        UIFont *font = titleTextAttributes[NSFontAttributeName];
-        UIColor *color = titleTextAttributes[NSForegroundColorAttributeName];
-        
-        nai.titleLable.font = font;
-        nai.titleLable.textColor = color;
-    }
-    [(WQNavActivityIndicatorView *)self.navigationItem.titleView start];
+    WQNavActivityIndicatorView *ai = [WQNavActivityIndicatorView wqNavActivityIndicatorViewWithTitle:@"获取中" textAttributes:self.navigationController.navigationBar.titleTextAttributes needIndicator:true];
+    self.navigationItem.titleView = ai;
 }
 - (void)hideNavActiviyIndicator {
-    [(WQNavActivityIndicatorView *)self.navigationItem.titleView stop];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.7 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        WQNavActivityIndicatorView *ai = [WQNavActivityIndicatorView wqNavActivityIndicatorViewWithTitle:self.navigationItem.title textAttributes:self.navigationController.navigationBar.titleTextAttributes needIndicator:false];
+        self.navigationItem.titleView = ai;
+    });
 }
 @end
--- testNavActivityIndicator/testNavActivityIndicator/WQNavActivityIndicator/WQNavActivityIndicatorView.swift
+++ testNavActivityIndicator/testNavActivityIndicator/WQNavActivityIndicator/WQNavActivityIndicatorView.swift
@@ -9,117 +9,40 @@
 import UIKit
 
 class WQNavActivityIndicatorView: UIView {
-    
-    private static let ANIMATING_TITLE: String? = "收取中"   //默认
-    
-    @objc var titleLable: UILabel!
-    
-    private var title: String!
-    
-    private var activityIndicatorView: UIActivityIndicatorView!
-    
-    @objc init(frame: CGRect, title: String) {
-        super.init(frame: frame)
-        
-        self.title = title
-        //设置大小
-        
-        self.frame = CGRect.init(x: 0, y: 0, width: 60, height: 44)
-        self.backgroundColor = UIColor.black
-        
-        //活动指示器
-        let ai = UIActivityIndicatorView.init(activityIndicatorStyle: UIActivityIndicatorViewStyle.white)
-        activityIndicatorView = ai
-        //        ai.startAnimating()
-        ai.frame = CGRect.init(x: 0, y: 6, width: 20, height: 20)
-        //                ai.backgroundColor = UIColor.purple
-        ai.transform = CGAffineTransform.init(scaleX: 1.2, y: 1.2)
-        //重新计算frame
-        var ai_frame = ai.frame
-        ai_frame.origin.x = 0
-        ai_frame.origin.y = (self.frame.height-ai_frame.height)/2
-        ai.frame = ai_frame
-        self.addSubview(ai)
-        
+
+    //工厂模式初始化
+    @objc class func wqNavActivityIndicatorView(title: String, textAttributes: [NSAttributedStringKey: Any], needIndicator: Bool = false) -> WQNavActivityIndicatorView {
+        var ai: UIActivityIndicatorView? = nil
+        if needIndicator {
+            //菊花
+            ai = UIActivityIndicatorView.init(activityIndicatorStyle: UIActivityIndicatorViewStyle.white)
+            ai?.frame = CGRect.init(x: 0, y: 0, width: 20, height: 20)
+            ai?.transform = CGAffineTransform.init(scaleX: 1.2, y: 1.2)
+            ai?.startAnimating()
+        }
         
         //标题
-        let titleLbl = UILabel.init(frame: CGRect.init(x: ai.frame.maxX+2, y: 0, width: 30, height: self.frame.height))
-        titleLable = titleLbl
-        titleLbl.textColor = UIColor.white
-        titleLbl.font = UIFont.systemFont(ofSize: 18)
-        self.addSubview(titleLbl)
+        let titleLbl = UILabel.init(frame: CGRect.init(x: (ai==nil ? 0: ai!.frame.maxX)+2, y: 0, width: 30, height: 44))
+        titleLbl.font = textAttributes[NSAttributedStringKey.font] as? UIFont
+        titleLbl.textColor = textAttributes[NSAttributedStringKey.foregroundColor] as? UIColor
         titleLbl.text = title
-        //        titleLbl.backgroundColor = UIColor.red
+//        titleLbl.backgroundColor = UIColor.red
         
-        
-        self.addSubview(titleLbl)
-        
-        self.resetTitleLabelFrame(x: nil)
-        self.resetFrame()
-        
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        //        fatalError("init(coder:) has not been implemented")
-    }
-    
-    @objc func start() {
-        if self.activityIndicatorView.isAnimating {
-            return
-        }
-        self.activityIndicatorView.startAnimating()
-        self.titleLable.text = WQNavActivityIndicatorView.ANIMATING_TITLE ?? self.title
-        //计算宽度
-        self.resetTitleLabelFrame(x: self.activityIndicatorView.frame.maxX)
-        self.resetFrame()
-    }
-    
-    @objc func stop() {
-        if self.activityIndicatorView.isAnimating == false {
-            return
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1) {
-            self.activityIndicatorView.stopAnimating()
-            self.titleLable.text = self.title
-            //计算宽度
-            self.resetTitleLabelFrame(x: 0)
-            self.resetFrame()
-        }
-        
-        
-    }
-    
-    private func resetTitleLabelFrame (x: CGFloat?) {
-        //适配宽度
-        let width = self.titleLable.sizeThatFits(CGSize.init(width: 1000, height: self.titleLable.frame.height)).width
-        var titleLblFrame = self.titleLable.frame
+        let width = titleLbl.sizeThatFits(CGSize.init(width: 1000, height: titleLbl.frame.height)).width
+        var titleLblFrame = titleLbl.frame
         titleLblFrame.size.width = width
-        if let x = x {
-            titleLblFrame.origin.x = x
-        }
-        self.titleLable.frame = titleLblFrame
-    }
-    
-    private func resetFrame() {
-        //设置标题view
-        var tvFrame = self.frame
+        titleLbl.frame = titleLblFrame
         
-        if (self.activityIndicatorView.isAnimating) {
-            
+        let x = (UIScreen.main.bounds.width-titleLbl.frame.maxX) / 2
+        let view = WQNavActivityIndicatorView.init(frame: CGRect.init(x: x, y: 0, width: titleLbl.frame.maxX, height: 44))
+        view.addSubview(titleLbl)
+        if let ai = ai {
+            view.addSubview(ai)
+            var center = ai.center
+            center.y = view.frame.height / 2
+            ai.center = center
         }
-        tvFrame.size.width = self.titleLable.frame.maxX
-        print("maxX = \(self.titleLable.frame.maxX)")
-        self.frame = tvFrame
+        
+        return view
     }
-    /*
-     // Only override draw() if you perform custom drawing.
-     // An empty implementation adversely affects performance during animation.
-     override func draw(_ rect: CGRect) {
-     // Drawing code
-     }
-     */
-    
 }
